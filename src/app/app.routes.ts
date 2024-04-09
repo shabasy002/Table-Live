70ea95d5b8e9ecc76f771deeb42f4d013b841a94
shabasy salim <shabasy.salim@assurant.com>
2024-04-09 15:03:54 +0530

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, UrlSegment } from '@angular/router';
+import {TableComponent} from '../app/components/table/table.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: TableComponent
+    }
+];
