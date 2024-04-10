0392fffbb1e47d3da32b5ee29c8603775a8c7585
shabasy salim <shabasy.salim@assurant.com>
2024-04-10 11:44:45 +0530

    second commit

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import {CdkTableModule, DataSource} from '@angular/cdk/table';
+import {BehaviorSubject, Observable} from 'rxjs';
+export interface UserTable {
+  name: string;
+  phone: number;
+  Address:string;
+}
+const ELEMENT_DATA: UserTable[] = [
+  { name: 'Hydrogen', phone: 1.0079, Address: 'Trivandrum'},
+  { name: 'Oxygen', phone: 2.0079, Address: 'Kollam'}
 
+ 
+];
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [],
+  imports: [CdkTableModule],
   templateUrl: './table.component.html',
   styleUrl: './table.component.scss'
 })
 export class TableComponent {
+  displayedColumns: string[] = [ 'name', 'phone', 'Address'];
+  dataSource = new ExampleDataSource();
+}
+export class ExampleDataSource extends DataSource<UserTable> {
+  /** Stream of data that is provided to the table. */
+  data = new BehaviorSubject<UserTable[]>(ELEMENT_DATA);
 
+  /** Connect function called by the table to retrieve one stream containing the data to render. */
+  connect(): Observable<UserTable[]> {
+    return this.data;
+  }
+
+  disconnect() {}
 }
