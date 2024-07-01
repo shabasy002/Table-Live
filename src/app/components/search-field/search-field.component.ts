ca5a5326038fd9fc5ec7052a16ea0a2f9c393bbb
shabasy salim <shabasy.salim@assurant.com>
2024-07-01 23:41:38 +0530

    form-field-local-storage

--- src/app/components/search-field/search-field.component.ts
+++ src/app/components/search-field/search-field.component.ts
@@ -1,11 +1,12 @@
-import { Component, Output, input, Input, EventEmitter, OnInit } from '@angular/core';
+import { Component, Output, input, Input, EventEmitter, OnInit, Inject, PLATFORM_ID, DoCheck } from '@angular/core';
 import { MatFormField } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { FormsModule, FormControl, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { FormsModule, FormControl, ReactiveFormsModule, FormGroup, FormBuilder } from '@angular/forms';
 import { TempTableComponent } from '../temp-table/temp-table.component';
 import {  OnDestroy } from '@angular/core';
 import { Subject } from 'rxjs';
 import { debounceTime, takeUntil } from 'rxjs/operators';
+import { DOCUMENT, isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-search-field',
   standalone: true,
@@ -13,36 +14,50 @@ import { debounceTime, takeUntil } from 'rxjs/operators';
   templateUrl: './search-field.component.html',
   styleUrl: './search-field.component.scss'
 })
-export class SearchFieldComponent implements OnInit, OnDestroy {
-  searchForm = new FormGroup({
-    nameFilter1 : new FormControl("")
-  });
-  @Input() public control: any;
+export class SearchFieldComponent implements OnInit, OnDestroy, DoCheck {
+  private fromLocalstorage:string='';
+  searchForm!: FormGroup;
+
+  // searchForm = new FormGroup({
+    
+  //   nameFilter1 : new FormControl(this.fromLocalstorage||'')
+  // });
+  //@Input() public control: any;
   @Output() searchText: string = '';
 
   private searchSubject = new Subject<string>();
   private readonly debounceTimeMs = 300; 
-  
-
   public valueTosend:any;
   @Output() searchEvent = new EventEmitter<string>();
-   debouncer: Subject<string> = new Subject<string>();
+  debouncer: Subject<string> = new Subject<string>();
   
- constructor(){
+ constructor( @Inject(DOCUMENT) private document: Document,
+ @Inject(PLATFORM_ID) private platformId: any, private fb: FormBuilder){
   this.debouncer
        .pipe(debounceTime(300))
        .subscribe((value) => this.searchEvent.emit(value));
  }
- ngOnInit() {
-
+ ngDoCheck(): void {
+  
  }
+ ngOnInit() {
+  if  (isPlatformBrowser(this.platformId)) {
+    this.searchForm = this.fb.group({
+    nameFilter1: [ localStorage.getItem('searchTerm') || ''] // Use an empty string if no value is found
+  });
+}
+else{
+  this.searchForm = this.fb.group({
+    nameFilter1: [''] // Use an empty string if no value is found
+  });
+}
+ }
+ 
 
  ngOnDestroy() {
 
  }
- nothing(){
 
- }
  getValue(){
   this.valueTosend=(this.searchForm.controls['nameFilter1'].value);
   this.debouncer.next(this.valueTosend);
