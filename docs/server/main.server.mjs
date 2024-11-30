ab2d812cb7e6f47c03dda70e4e4d12650dcb40eb
shabasy salim <shabasy.salim@assurant.com>
2024-11-30 22:16:52 +0530

    Initial commit

---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UONKXS7P.mjs";import"./chunk-BVM26RPK.mjs";import"./chunk-2CEAJHK5.mjs";import"./chunk-EPLZZUPT.mjs";import"./chunk-IMQYJ23T.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/services/api-call.service.ts
+++ src/app/services/api-call.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -7,7 +8,7 @@ export class ApiCallService {
 
   constructor(private http:HttpClient) { }
 
-  getPost(){
+  getPost():Observable<any[]>{
     return this.http.get<any>("https://jsonplaceholder.typicode.com/users");
   }
 }
